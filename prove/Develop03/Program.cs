f006b43c264092f76c7d1be0db804d992ad7ed77
isJosephAd <123178147+isJosephAD@users.noreply.github.com>
2023-05-28 14:10:41 +0400

    all

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,35 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture("Proverbs 3:5-6", "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.");
+
+        
+
+        while(!scripture.AllWordsHidden){
+
+            Console.Clear();    
+            scripture.Display();
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to hide words or type 'quit' to exit.");
+            
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit"){
+                break;
+            }
+            else{
+                scripture.HideRandomWord();
+            }
+        }
+
+        Console.Clear();
+        scripture.DisplayHidden();
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
     }
 }
